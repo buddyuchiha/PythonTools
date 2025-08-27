4026fc585286c1a1c8bc3481a320bfd0ba09693c
Buddy <Pro100biv@mail.ru>
2025-08-27 18:51:05 +0400

    project complete

--- src/app/services/LogsValidater.py
+++ src/app/services/LogsValidater.py
@@ -1,9 +1,11 @@
 import re 
 
+from core.config import config
 
-class LogsValidater:
-    def __init__(self, template: re) -> None:
-        self.template = template
+
+class LogsValidater:    
+    def __init__(self) -> None:
+        self.template = config['TEMPLATES']['LOG_TEMPLATE']
     
     def __report_validate(
         self,
@@ -22,7 +24,7 @@ class LogsValidater:
                 f"Содержимое: {data[1]}"
             )
 
-    def __validater(self, file_data: str) -> str:
+    def __validater(self, file_data: str) -> tuple[int, int, list]:
         incorrect_list = []
         line_num = 0
 
@@ -36,12 +38,88 @@ class LogsValidater:
 
         return correct_num, incorrect_num, incorrect_list
 
-    def validate(self, file_path: str) -> str:
-        with open(file_path, "r", encoding='utf-8') as file:
-            correct_num, incorrect_num, incorrect_list = self.__validater(file)
+    def validate(self, file_path: str) -> str:     
+        try:   
+            with open(file_path, "r", encoding='utf-8') as file:
+                correct_num, incorrect_num, incorrect_list = \
+                    self.__validater(file)
 
-            return self.__report_validate(
+            self.__report_validate(
                 correct_num, 
                 incorrect_num, 
                 incorrect_list
                 )
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Wrong path: {file_path}")
+
+    def __get_statistics(self, data: str) -> list:
+        info_list = re.findall(r"\[INFO\]", data)
+        warning_list = re.findall(r"\[WARNING\]", data)
+        error_list = re.findall(r"\[ERROR\]", data)
+        debug_list = re.findall(r"\[DEBUG\]", data)
+
+        statistics = [
+            ['INFO', len(info_list)],
+            ['WARNING', len(warning_list)],
+            ['ERROR', len(error_list)],
+            ['DEBUG', len(debug_list)]
+        ]
+
+        return statistics
+
+    def __analyzer(self, file_data: str) -> tuple[int, list, list]:
+        correct_num = 0
+        correct_lines = []
+
+        for line in file_data:
+            strip_line = line.strip()
+
+            if re.fullmatch(self.template, strip_line):
+                correct_num += 1
+                correct_lines.append(line)
+
+        correct_file_data = " ".join(correct_lines)
+
+        timedate_list = re.findall(
+            r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}", correct_file_data
+            )
+
+        statistics = self.__get_statistics(correct_file_data)
+
+        return correct_num, statistics, timedate_list
+
+    def __report_analyze(
+        self,
+        correct_num: int, 
+        statistics: list, 
+        time_range: list   
+        ) -> None:
+
+        print(f"Общее количество корректных записей: {correct_num}")
+        print("Статиска по уровням логирования")
+
+        for statistic in statistics:
+            print(
+                f"Уровень логгирования: {statistic[0]} \n", 
+                f"Количество записей: {statistic[1]}"
+                )
+
+        print(
+            f"Самая раняя дата записи: {time_range[0]} \n",
+            f"Самая поздняя дата записи: {time_range[1]}"            
+            )
+
+    def analyze(self, file_path: str) -> str:
+        try: 
+            with open(file_path, "r", encoding='utf-8') as file:
+                file_data = file.readlines()
+                correct_num, statistics, time_range = \
+                    self.__analyzer(file_data) 
+
+            self.__report_analyze(
+                correct_num,
+                statistics, 
+                time_range
+            )
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Wrong path: {file_path}")
--- src/main.py
+++ src/main.py
@@ -1,9 +1,42 @@
+import argparse
+
 from app.services.LogsValidater import LogsValidater
 from core.config import config
 
 
-if __name__ == "__main__":
-    raw_string = r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3} \[\w{1,7}\] \[[a-z_-]+\] .+"
-    lv = LogsValidater(raw_string)
+def main():
+    lv = LogsValidater()
+    
+    parser = argparse.ArgumentParser(description='LogsValidater')
 
-    lv.validate(config['PATH']['BASIC_LOGS'])
+    parser.add_argument('-v', '--validate', help='Logs validation')
+    parser.add_argument('-a', '--analyze', help='Logs analyze')
+    parser.add_argument('-t', '--test', help='Default tests')
+
+    args = parser.parse_args()
+
+    if args.validate:   
+        print('Валидацмя логов')
+        path = args.validate 
+        lv.validate(path)
+
+    if args.analyze:
+        print('Анализ логов')
+        path = args.analyze
+        lv.analyze(path)
+
+    if args.test:
+        print("Базовый тест всех функций")
+
+        lv.validate(config['PATH']['BASIC_LOGS'])
+        lv.analyze(config['PATH']['ANALYSIS_LOGS'])
+        
+        print("-"*30)
+        print("Продвинутый тест всех функций")
+        
+        lv.validate(config['PATH']['EDGE_CASES_LOGS'])
+        lv.analyze(config['PATH']['EDGE_CASES_LOGS'])
+
+
+if __name__ == "__main__":
+    main()
